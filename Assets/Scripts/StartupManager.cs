15554dbc6c0fcf544d640d202dd7cca7e4604cc1
Denis <khusainov02@icloud.com>
2022-06-20 13:40:01 +0300

    Created StartupManager and UI Manager

---
+++ Assets/Scripts/StartupManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartupManager : MonoBehaviour
+{
+    private const string StartupScene = "StartUp";
+
+    private const string TutorialLevel = "SampleScene";
+
+    private const string UIScene = "UI";
+
+    IEnumerator Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        Application.targetFrameRate = 60;
+
+        yield return StartCoroutine(LoadScene());
+
+        Destroy(gameObject);
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return SceneManager.LoadSceneAsync(UIScene);
+
+        yield return SceneManager.LoadSceneAsync(TutorialLevel);
+    }
+}
