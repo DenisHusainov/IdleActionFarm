6a115aad9e0cd97b69d7e2b45d7006d3680691c0
Denis <khusainov02@icloud.com>
2022-06-23 09:55:17 +0300

    Fixed GrassUp animation

--- Assets/Scripts/GrassSpawn.cs
+++ Assets/Scripts/GrassSpawn.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class GrassSpawn : MonoBehaviour
 {
-    private const float _startPosX = -3.9f;
+    private const float _startPosX = 0f;
     private const float _startPosZ = 30f;
-    private const float _startPosY = 0.9f;
+    private const float _startPosY = -0.3f;
     private const float _stepX = 1f;
     private const float _stepZ = 2f;
     
@@ -12,8 +12,7 @@ public class GrassSpawn : MonoBehaviour
     private GameObject _grassPrefab = null;
     [SerializeField] 
     private GameObject _parentObject = null;
-    
-    
+
     void Start()
     {
         for (var i = 0; i < 4; i++)
